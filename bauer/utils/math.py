354bcba5f78d16b8e970b0e75ec429006759b5ee
Gilles de Hollander <gilles.de.hollander@gmail.com>
2023-04-28 17:09:07 +0200

    Convenience math fucntions

--- bauer/utils/math.py
+++ bauer/utils/math.py
@@ -1,3 +1,4 @@
+import numpy as np
 import aesara.tensor as at
 
 def logistic(x):
@@ -5,3 +6,7 @@ def logistic(x):
 
 def inverse_softplus(x):
     return at.log(at.exp(x) - 1.)
+
+
+def softplus_np(x): return np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0)
+def logistic_np(x): return 1 / (1 + np.exp(-x))
