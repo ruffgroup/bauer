7b760d46c012370f365ea0ac9e810ec140786061
Gilles de Hollander <gilles.de.hollander@gmail.com>
2023-07-25 15:45:03 +0200

    Inverse softplus using np

--- bauer/utils/math.py
+++ bauer/utils/math.py
@@ -9,4 +9,8 @@ def inverse_softplus(x):
 
 
 def softplus_np(x): return np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0)
+
+def inverse_softplus_np(x):
+    return np.log(np.exp(x) - 1.)
+
 def logistic_np(x): return 1 / (1 + np.exp(-x))
